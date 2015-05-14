3e779069070ee8b660591ccfb9a3aee73001e9c7
sylvia.seow <sylvia.seow@mbww.com>
2015-05-14 16:38:06 +0800

    Sylvia Assignment2
    with comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Cache the inverse of matrix using concept lexical scooping
+## Recalculate can be based on the cache copy rather than having to calculate the inverse of matrix again
+
 ## Write a short comment describing this function
+## this function will create the special matrix and store it as cache copy
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,7 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##This function calculate the inverse of the special "matrix" returned by makeCacheMatrix function. 
+##If the inverse has already been cached and calculated (and the matrix stayed the same), 
+##then the cachesolve function should retrieve the inverse value from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getsolve()
